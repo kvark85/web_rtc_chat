2d0df731f810b8963cff1a5cb59c1196b15e801c
b.andrey <b.andrey@codewizard.team>
2024-05-25 11:58:40 +0200

    Chore: Changes for debug 12

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,7 +1,6 @@
 const ICE_config= {
     'iceServers': [
         {'url': 'stun:stun.l.google.com:19302'},
-        {'url': 'stun.l.google.com:19302'},
         {'url': 'stun1.l.google.com:19302'},
         {'url': 'stun2.l.google.com:19302'},
         {'url': 'stun3.l.google.com:19302'},
