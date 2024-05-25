0d9d4a74dc83d0cffb2317106d5837e00426b364
b.andrey <b.andrey@codewizard.team>
2024-05-25 11:33:45 +0200

    Chore: Changes for debug 4

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,5 +1,21 @@
-const configuration = {'iceServers': [{'urls': 'stun:stun.l.google.com:19302'}]}
-const peerConnection = new RTCPeerConnection(configuration);
+const ICE_config= {
+    'iceServers': [
+        {
+            'url': 'stun:stun.l.google.com:19302'
+        },
+        {
+            'url': 'turn:192.158.29.39:3478?transport=udp',
+            'credential': 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+            'username': '28224511:1379330808'
+        },
+        {
+            'url': 'turn:192.158.29.39:3478?transport=tcp',
+            'credential': 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+            'username': '28224511:1379330808'
+        }
+    ]
+}
+const peerConnection = new RTCPeerConnection(ICE_config);
 const socket = io.connect("/");
 
 const iceCandidates = []
