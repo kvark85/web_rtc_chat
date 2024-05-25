4ca7cb7d53c4506f44f0ed7a9e458ee0f94ef168
b.andrey <b.andrey@codewizard.team>
2024-05-25 12:13:36 +0200

    Chore: Changes for debug 19

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -16,17 +16,27 @@ const ICE_config= {
 const GEO_LOC_URL = "https://raw.githubusercontent.com/pradt2/always-online-stun/master/geoip_cache.txt";
 const IPV4_URL = "https://raw.githubusercontent.com/pradt2/always-online-stun/master/valid_ipv4s.txt";
 const GEO_USER_URL = "https://geolocation-db.com/json/";
-const geoLocs = await(await fetch(GEO_LOC_URL)).json();
-const { latitude, longitude } = await(await fetch(GEO_USER_URL)).json();
-const closestAddr = (await(await fetch(IPV4_URL)).text()).trim().split('\n')
-    .map(addr => {
-        const [stunLat, stunLon] = geoLocs[addr.split(':')[0]];
-        const dist = ((latitude - stunLat) ** 2 + (longitude - stunLon) ** 2 ) ** .5;
-        return [addr, dist];
-    }).reduce(([addrA, distA], [addrB, distB]) => distA <= distB ? [addrA, distA] : [addrB, distB])[0];
-console.log('closestAddr', closestAddr); // prints the IP:PORT of the closest STUN server
+
+const getLocation = async () => {
+    const geoLocs = await(await fetch(GEO_LOC_URL)).json();
+    const { latitude, longitude } = await(await fetch(GEO_USER_URL)).json();
+    const closestAddr = (await(await fetch(IPV4_URL)).text()).trim().split('\n')
+        .map(addr => {
+            const [stunLat, stunLon] = geoLocs[addr.split(':')[0]];
+            const dist = ((latitude - stunLat) ** 2 + (longitude - stunLon) ** 2 ) ** .5;
+            return [addr, dist];
+        }).reduce(([addrA, distA], [addrB, distB]) => distA <= distB ? [addrA, distA] : [addrB, distB])[0];
+    console.log('closestAddr', closestAddr); // prints the IP:PORT of the closest STUN server
+    return closestAddr
+}
+
+getLocation().then((closestAddr) => {
+    console.log('222closestAddr', closestAddr);
+})
+
 const peerConnection = new RTCPeerConnection(ICE_config);
 const socket = io.connect("/");
+
 
 const iceCandidates = []
 
